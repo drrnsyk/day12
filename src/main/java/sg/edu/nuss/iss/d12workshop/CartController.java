3ab1f26c1d7d3fd851fd90a51d80dda4e98f3c7d
drrnsyk <darren.syk@gmail.com>
2022-07-19 14:33:27 +0800

    path variable added for cart and weather controller

--- src/main/java/sg/edu/nuss/iss/d12workshop/CartController.java
+++ src/main/java/sg/edu/nuss/iss/d12workshop/CartController.java
@@ -1,10 +1,12 @@
 package sg.edu.nuss.iss.d12workshop;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +23,24 @@ public class CartController {
         model.addAttribute("cartItems", cartItems); // attribute name
 
         // return html view
+        return "cart";
+    }
+
+    // create a path variable method
+    @GetMapping("{itemname}")
+    public String filteredCart(
+        @PathVariable(name="itemname", required=true) String itemname,
+        Model model) {
+        CartService cs = new CartService();
+        List<Item> cartItems = cs.getCartItems();
+
+        List<Item> filteredItems = new LinkedList<>();
+        for (Item i : cartItems) {
+            if (i.getItemName().contains(itemname))
+                filteredItems.add(i);
+        }
+
+        model.addAttribute("cartItems", filteredItems);
         return "cart";
     }
     
--- src/main/java/sg/edu/nuss/iss/d12workshop/WeatherController.java
+++ src/main/java/sg/edu/nuss/iss/d12workshop/WeatherController.java
@@ -3,6 +3,7 @@ package sg.edu.nuss.iss.d12workshop;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,5 +24,18 @@ public class WeatherController {
         return "weather";
         
     }
+
+    // method for path variable
+    @GetMapping("{city}")
+    public String weather2(
+        @PathVariable(name="city", required = true) String city, 
+        @RequestParam(name="units", defaultValue="kilometers") String units,
+        Model model) {
+
+            model.addAttribute("city", city);
+            model.addAttribute("units", units);
+
+            return "weather";
+        }
 
 }
